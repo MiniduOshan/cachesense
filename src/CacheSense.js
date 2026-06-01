95edadb1d6e60bb71ba33f336dad356463359a97
Minidu Oshan <126508461+MiniduOshan@users.noreply.github.com>
2026-06-01 12:49:24 +0530

    feat: implement cache wrapper

---
+++ src/CacheSense.js
@@ -0,0 +1,30 @@
+const Metrics = require("./Metrics");
+
+class CacheSense {
+	constructor(cache) {
+		this.cache = cache;
+		this.metrics = new Metrics();
+	}
+
+	get(key) {
+		const value = this.cache.get(key);
+
+		if (value !== undefined) {
+			this.metrics.recordHit();
+		} else {
+			this.metrics.recordMiss();
+		}
+
+		return value;
+	}
+
+	set(key, value) {
+		this.cache.set(key, value);
+	}
+
+	stats() {
+		return this.metrics.getStats();
+	}
+}
+
+module.exports = CacheSense;
